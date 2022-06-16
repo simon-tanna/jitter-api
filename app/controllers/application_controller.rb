70533f3f8aee9aac02be3734a1e34bd7bcd65bf7
simon-tanna <gcab022109@coderacademy.edu.au>
2022-06-16 11:01:52 +1000

    set up knock auth. created users model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::API
+    include Knock::Authenticable # all controllers can use knock
 end
--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ Bundler.require(*Rails.groups)
 module JitterApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 6.1 and config.autoloader = :classic
+    # config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
