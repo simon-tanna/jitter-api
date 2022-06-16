42b9d40382add44979ac1e53ea5a645afd869a05
simon-tanna <gcab022109@coderacademy.edu.au>
2022-06-16 11:56:14 +1000

    created signin method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,18 @@ class UsersController < ApplicationController
         end
     end
 
+    def sign_in
+        @user = User.find_by_email(params[:email])
+
+        if @user && @user.authenticate(params[:password])
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+            render json: {username: @user.username, jwt: auth_token.token}, status: 200
+        else 
+            render json: {error: 'Invalid username or password'}, status: 401
+        end
+    end
+
+
     private 
     def user_params
         params.permit(:username, :email, :password, :password_confirmation)
