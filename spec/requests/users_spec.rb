5c4ed9a6fa557e6e528ee89a17a91ff2c33b8794
simon-tanna <gcab022109@coderacademy.edu.au>
2022-06-30 11:29:53 +1000

    added test for user login request

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,10 +12,17 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it "does not log the user in with incorrect credentials" do
-      post '/auth/signin', params: { email: "test2@example.com", password: "123436"}
+    it "does not log the user in with incorrect password" do
+      post '/auth/signin', params: { email: "test3@example.com", password: "123436"}
       expect(response).to have_http_status(401)
       expect(response.body).to include("Invalid username or password")
     end
+
+    it "does not log the user in with non valid email" do
+      post '/auth/signin', params: { email: "te@example.com", password: "123436"}
+      expect(response).to have_http_status(401)
+      expect(response.body).to include("Invalid username or password")
+    end
+
   end
 end
